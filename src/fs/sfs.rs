c35c33a7755db098ac9eae3ca14ae4d20e10cf0f
simondevlp <tuongphat2505@gmail.com>
2025-10-22 22:33:10 +0800

    Moved SFs GUID to its implementation

--- src/fs/sfs.rs
+++ src/fs/sfs.rs
@@ -1,7 +1,16 @@
-use crate::{fs::file, status::Status};
+use crate::{fs::file, guid::Guid, status::Status};
 
 #[repr(C)]
 pub struct Protocol {
     pub revision: u64,
     pub open_volume: extern "efiapi" fn(this: &Self, root: &mut *mut file::Protocol) -> Status,
+}
+
+impl Protocol {
+    pub const GUID: Guid = Guid::new(
+        0x0964e5b22,
+        0x6459,
+        0x11d2,
+        [0x8e, 0x39, 0x00, 0xa0, 0xc9, 0x69, 0x72, 0x3b],
+    );
 }
